35bf9d456d1d108eb0dc87f15b5f83c79f133db3
gourabsen21s <gourabsen.21.2001@gmail.com>
2025-03-14 20:13:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connect from "./db/db.js";
 import userRoutes from "./routes/user.routes.js"
+import aiRoutes from './routes/ai.routes.js';
 import projectRoutes from './routes/project.routes.js';
 dotenv.config();
 
@@ -14,12 +15,13 @@ app.get("/", (req, res) => {
   res.send("Backend is running successfully!");
 });
 
-app.use(cors({ origin: "https://lively-sand-0a321151e.6.azurestaticapps.net", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/users',userRoutes);
 app.use('/projects', projectRoutes);
+app.use("/ai", aiRoutes)
 
 export default app;
